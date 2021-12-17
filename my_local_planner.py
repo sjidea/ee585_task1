538404bb316aa6e71b7a6c61dc5e9a727db5cd66
sjidea <sonsojung99@gmail.com>
2021-12-17 23:51:20 +0900

    path in

--- my_local_planner.py
+++ my_local_planner.py
@@ -31,7 +31,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
                 "/")
 
 try:
-    from frenet_optimal_planning import FrenetPath
+    from frenet_optimal_trajectory import FrenetPath
 except ImportError:
     raise
 
