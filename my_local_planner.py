800b15d9946cd45fa7816389e1fb4e8671bf677b
sjidea <sonsojung99@gmail.com>
2021-12-18 19:21:45 +0900

    path in

--- my_local_planner.py
+++ my_local_planner.py
@@ -374,6 +374,7 @@ class MyLocalPlanner(object):
         except:    
             print('ob is the problem')
         obs = np.array(obs)
+        print("obstacle position {}", obs)
         
         try:
             path = frenet_optimal_trajectory.frenet_optimal_planning(self.csp, self.s0, self.c_speed, self.c_d, self.c_d_d, self.c_d_dd, \
