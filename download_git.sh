359de9ecbcea52fe58ad292d4fa802561fb4904a
sjidea <sonsojung99@gmail.com>
2021-12-18 14:06:27 +0900

    path in

--- download_git.sh
+++ download_git.sh
@@ -1,7 +1,7 @@
 clear
 rm -rf CubicSpline/ ee585_task1/
-rm frenet_optimal_trajectory.py quintic_polynomials_planner.py CubicSpline/cubic_spline_planner.py
-rm frenet_optimal_trajectory.pyc quintic_polynomials_planner.pyc CubicSpline/cubic_spline_planner.pyc
+rm frenet_optimal_trajectory.py quintic_polynomials_planner.py #CubicSpline/cubic_spline_planner.py
+rm frenet_optimal_trajectory.pyc quintic_polynomials_planner.pyc #CubicSpline/cubic_spline_planner.pyc
 git clone https://github.com/sjidea/ee585_task1.git
 mv ee585_task1/* .
 rm output.txt
