1277b505988ec3e6873225aa19632cc2b0b5dd0d
sjidea <sonsojung99@gmail.com>
2021-12-18 12:26:28 +0900

    path in

--- my_local_planner.py
+++ my_local_planner.py
@@ -277,7 +277,7 @@ class MyLocalPlanner(object):
         self._waypoints_queue.clear()
         for i, elem in enumerate(current_plan):
         #----- to eliminate repeted points
-            if not (i ==1):
+            if not isempty(self._waypoints_queue):
                 rospy.loginfo("waypoints queue = {}, idx = {}".format(self._waypoints_queue, i))
                 prev_ = self._waypoints_queue[-1].position
                 curr_ = elem.pose.position
