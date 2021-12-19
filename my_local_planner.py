7063b9656cd82d7363d298d2c6095e646a34a023
sjidea <sonsojung99@gmail.com>
2021-12-19 17:10:06 +0900

    day 2

--- my_local_planner.py
+++ my_local_planner.py
@@ -409,9 +409,9 @@ class MyLocalPlanner(object):
 
         # target waypoint        
         self.target_route_point = self._waypoint_buffer[0]
-        if path:
-            self.target_route_point.position.x = path.x[1]
-            self.target_route_point.position.y = path.y[1]
+        # if path:
+        self.target_route_point.position.x = path.x[1]
+        self.target_route_point.position.y = path.y[1]
         target_point = PointStamped()
         target_point.header.frame_id = "map"
         target_point.point.x = self.target_route_point.position.x
