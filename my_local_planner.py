ac54ea2bc62e698ee06937d63e1d089adba7cfab
sjidea <sonsojung99@gmail.com>
2021-12-20 00:03:20 +0900

    seperate obstacles

--- my_local_planner.py
+++ my_local_planner.py
@@ -405,11 +405,10 @@ class MyLocalPlanner(object):
         #     target_speed = 50
         # else:
         #     target_speed = 30
-        path = []
-        if len(path):
-            path = frenet_optimal_trajectory.frenet_optimal_planning( \
-                                self.csp, self.s0, self.c_speed, self.c_d, self.c_d_d, self.c_d_dd, \
-                                obs)
+        # path = []
+        path = frenet_optimal_trajectory.frenet_optimal_planning( \
+                            self.csp, self.s0, self.c_speed, self.c_d, self.c_d_d, self.c_d_dd, \
+                            obs)
 
         # target waypoint        
         self.target_route_point = self._waypoint_buffer[0]
