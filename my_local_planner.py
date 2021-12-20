bc87f8d2e2c4727dbb1f34936394c3595896c388
sjidea <sonsojung99@gmail.com>
2021-12-20 10:40:06 +0900

    seperate obstacles

--- my_local_planner.py
+++ my_local_planner.py
@@ -441,7 +441,7 @@ class MyLocalPlanner(object):
                     target_speed = 80
             # elif 
             else:
-                target_speed = 30
+                target_speed = 25
 
 
         # move using PID controllers
